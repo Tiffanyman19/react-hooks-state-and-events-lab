971b94bf7bd0e7b9996f3d1ba93c840439fde12d
Tiffany Man <tiffanyman19@yahoo.coom>
2024-01-30 09:10:38 -0500

    Complete Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [Added, setAdded] = useState(false);
+  function changeAdded() {
+    setAdded(!Added);
+  }
   return (
-    <li className="">
+    <li className={Added ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={changeAdded}>
+        {Added ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
 
-export default Item;
+export default Item; 
